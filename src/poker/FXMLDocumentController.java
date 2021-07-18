ca4aead666b5dd39c8a4fba2882d51be8b220938
azuma95 <8ctiagopereira@gmail.com>
2021-07-18 20:02:54 +0100

    menu e mesa de jogo

--- src/poker/FXMLDocumentController.java
+++ src/poker/FXMLDocumentController.java
@@ -5,13 +5,19 @@
  */
 package poker;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
 /**
  *
@@ -25,6 +31,15 @@ public class FXMLDocumentController implements Initializable {
     @FXML
     private void handleClose(MouseEvent event) {
         System.exit(0);
+    }
+    
+    @FXML
+    private void handleButtonAction(ActionEvent event) throws IOException {
+        Parent mesaJogo = FXMLLoader.load(getClass().getResource("MesaJogo.fxml"));
+        Scene mesaJogoScene = new Scene(mesaJogo); 
+        Stage appMesa = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        appMesa.setScene(mesaJogoScene);
+        appMesa.show();
     }
     
     @Override
